e642579eeea42d4fb287409f7e95716981bf1681
abdelmalekbenamar <abdelmalek.benamar1990@gmail.com>
2024-11-02 15:33:28 +0100

    js pour boutton add edit save cancel

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const ajouterTache = document.querySelector(".ajouterTache");
 const sortTime = document.querySelector(".sortByDate");
 const sortTitle = document.querySelector(".sortByTitle");
-const deleteButt = document.querySelector(".deleteButton");
-const editButt = document.querySelector(".editButton");
+
 const cancelAdd = document.getElementById("cancelAdd");
 const saveAdd = document.getElementById("saveAdd");
+
+const cancelEdit = document.getElementById("cancelEdit");
 
 const prio1Task = document.getElementById("taskUn");
 const prio2Task = document.getElementById("taskDeux");
@@ -14,12 +15,19 @@ const prio3Task = document.getElementById("taskTrois");
 
 
 ajouterTache.addEventListener("click", () =>{
-    const addForm = document.querySelector("#addItem"); 
+    const addForm = document.getElementById("addItem");
     addForm.classList.remove("hidden");
 });
 cancelAdd.addEventListener("click", () =>{
     document.getElementById("addItem").classList.add("hidden");
 });
+cancelEdit.addEventListener("click", () =>{
+    const editForm = document.getElementById("editTask");
+    editForm.classList.add("hidden")
+})
+
+
+
 saveAdd.addEventListener("click", (e) =>{
     e.preventDefault();
     const titreTache = document.getElementById("titreTache");
@@ -39,6 +47,8 @@ saveAdd.addEventListener("click", (e) =>{
         now = now.split(" ");
         now = now.join("");
         now +=time;
+
+    // create a div element
     
     switch(priority.value){
         case "p1":
@@ -51,39 +61,128 @@ saveAdd.addEventListener("click", (e) =>{
             bg = "bg-lime-600";
             break;
     }
-
-    const cardCode = `
-        <div id="${now}" class="tache ${bg} p-2 m-1 rounded-lg">
+                
+    let codeCard = document.createElement("div");
+    codeCard.className = `${bg} tache p-2 m-1 rounded-lg`
+    codeCard.id = now;
+    codeCard.innerHTML = `
                     <div class="titleTime flex justify-between">
-                        <h2 class="taskTitle font-bold">${titreTache.value}</h2>
-                        <p class="taskTime font-bold">${date.value}</p>
+                        <h2 id="taskAddedTitle" class="taskTitle font-bold">${titreTache.value}</h2>
+                        <p id="taskAddedTimeId" class="taskAddedTime font-bold">${date.value}</p>
                     </div>
-                    <p>${description.value}</p>
+                    <p id="descriptionAddedTask">${description.value}</p>
                     <div class="editDel w-fit ml-auto mr-auto">
                         <button class="editButton bg-green-900 rounded-lg p-2 text-white">Edit</button>
                         <button class="deleteButton bg-red-900 rounded-lg p-2 text-white">Delete</button>
-                    </div>
-                </div>
-    `;
+                    </div>`;
     
     switch(status.value){
         case "backlog":
-            prio1Task.innerHTML += cardCode;
+            prio1Task.appendChild(codeCard);
             break;
         case "inProgress":
-            prio2Task.innerHTML += cardCode;
+            prio2Task.appendChild(codeCard);
             break;
         case "done":
-            prio3Task.innerHTML += cardCode;
+            prio3Task.appendChild(codeCard);
             break;
     }
+    document.querySelector("#addItem").classList.add("hidden");
+    
+    const deleteButt = codeCard.querySelector(`#${now} .deleteButton`);
+    deleteButt.addEventListener("click", () =>{
+        deleteButt.parentNode.parentNode.remove();
+    })
+    
+    // create edit button event
+  
+ 
+    const editButt = codeCard.querySelector(`#${now} .editButton`);
+    editButt.addEventListener("click", () =>{
+        const editTask = document.getElementById("editTask");
+        editTask.classList.remove("hidden");
+        const titleTaskEdit = document.getElementById("titreTacheEdit")
+        const titleTaskEditDataNode = document.querySelector(`#${now} #taskAddedTitle`);
+        const descTaskEdit = document.getElementById("descriptionEdit");
+        const descTaskEditDataNode = document.querySelector(`#${now} #descriptionAddedTask`);
+        const dateTaskEdit = document.getElementById("dateEdit");
+        const dateTaskEditDataNode = document.querySelector(`#${now} #taskAddedTimeId`);
+        const prioTaskEdit = document.getElementById("priorityEdit1");
+        const prioTaskEditDataNode = document.querySelector(`#${now}`);
+        const prioBg = [];
+        
+
+        dateTaskEdit.value = dateTaskEditDataNode.innerText
+        descTaskEdit.value = descTaskEditDataNode.innerText;
+        titleTaskEdit.value = titleTaskEditDataNode.innerText;
+        prioBg.push(prioTaskEditDataNode.className.toString());
+        // prioBg.shift();
+        let classList = prioBg[0]
+        let classListArray = classList.split(" ");
+        let shiftedBg = classListArray.shift();
+        //la classe sans le background
+        // classListArray = classListArray.join(" ");
+        switch(shiftedBg){
+            case "bg-red-800":
+                prioTaskEdit.value = "priority1";
+            break;
+        case "bg-orange-600":
+            prioTaskEdit.value = "priority2";
+            break;
+        case "bg-lime-600":
+            prioTaskEdit.value = "priority3";
+            break;
+        }
+
+
+        const saveEdit = document.getElementById("saveEdit");
+        saveEdit.addEventListener("click", (e) =>{
+            e.preventDefault();
+            const editNewTitle = document.getElementById("titreTacheEdit");
+            const editNewDesc = document.getElementById("descriptionEdit");
+            const editNewDate = document.getElementById("dateEdit");
+            const editNewPrio = document.getElementById("priorityEdit1");
+
+            const taskToEdit = document.getElementById(`${now}`);
+            const titleToEdit = taskToEdit.querySelector("#taskAddedTitle");
+            const descToEdit = taskToEdit.querySelector("#descriptionAddedTask");
+            const dateToEdit = taskToEdit.querySelector("#taskAddedTimeId");
+
+            titleToEdit.innerText = editNewTitle.value;
+            descToEdit.innerText = editNewDesc.value;
+            dateToEdit.innerText = editNewDate.value;
+            switch(editNewPrio.value){
+                case "priority1":
+                    classListArray.unshift("bg-red-800");
+                    break;
+                case "priority2":
+                    classListArray.unshift("bg-orange-600");
+                    break;
+                case "priority3":
+                    classListArray.unshift("bg-lime-600");
+                    break;
+
+            }
+            let laClassChangeBg = classListArray.join(" ");
+            taskToEdit.className = laClassChangeBg;
+            document.getElementById("editTask").classList.add("hidden");
+            console.log(editNewPrio.value);
+        })
+        
+        
+    })
+
+
+
     titreTache.value = "";
     date.value = "";
     description.value = "";
-    document.querySelector("#addItem").classList.add("hidden")
-    
+    })
 
-})
+   
+
+
+
 
 
 
